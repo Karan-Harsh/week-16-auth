5d1771dbcb57b523f62481cb7277237c86a652d9
Harsh Karan <karanharshe577@gmail.com>
2024-04-11 23:43:17 +0530

    global routes

---
+++ app/api/auth/[...authRoutes]/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(
+  req: NextRequest,
+  { params }: { params: { authRoutes: string[] } }
+) {
+  console.log(params);
+  return NextResponse.json({
+    message: "asd",
+  });
+}
