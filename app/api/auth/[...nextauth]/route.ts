239ab6606dfb86e86cf95211bad9ca10c0e25b1e
Harsh Karan <karanharshe577@gmail.com>
2024-04-15 20:24:18 +0530

    new

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,27 @@
 import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "Email" },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials: any) {
+        console.log(credentials);
+        return {
+          id: "user1",
+          name: "Harsh Karan",
+        };
+      },
+    }),
+  ],
 });
 
 export const GET = handler;
